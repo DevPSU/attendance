402d3d4dd52aeea550f1ea3f5a8b03db9a4d579f
Haosen Wang <hxw5204@gmail.com>
2019-09-28 01:35:24 -0400

    Revert "sample"
    This reverts commit 5bd51e5d01aa66397e87112878578d26a4b412fc.

--- AQUI/App.js
+++ AQUI/App.js
@@ -1,49 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from "react-navigation";
+import { createStack}
 
-import { AppLoading, Asset} from "expo";
-
-import Navigation from './navigation';
-import { Block } from './components';
-import * as constants from './constants';
-import {render} from "react-native-web";
-
-// import all used images
-const images = [
-    require('./assets/images/plants_1.png'),
-    require('./assets/images/plants_2.png'),
-    require('./assets/images/explore_1.png'),
-    require('./assets/images/explore_2.png'),
-    require('./assets/images/explore_3.png'),
-    require('./assets/images/explore_4.png'),
-    require('./assets/images/explore_5.png'),
-    require('./assets/images/explore_6.png'),
-    require('./assets/images/avatar.png'),
-];
-
-export default class App extends React.Component {
-  state = {
-    isLoadingComplete:false,
-  }
-
-  handleResourceAsync = async () => {
-    // we're caching all the images
-    // for better performance on the app
-
-    const cacheImages = images.map(img => {
-      return Asset.fromModule(image).downloadAsync()
-    });
-
-    return Promise.all(cacheImages);
-  }
-
-  render() {
-    return (
-        <View style={styles.container}>
-          <Navigation/>
-        </View>
-    );
-  }
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({

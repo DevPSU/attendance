749d73e0eebf1fe8beae0766dd3ffa3fe572ed37
Haosen Wang <hxw5204@gmail.com>
2019-09-27 23:05:12 -0400

    build the structure

--- AQUI/App.js
+++ AQUI/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from "react-navigation";
+import { createStack}
 
 export default function App() {
   return (
